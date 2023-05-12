ff3db9c5372df8781bdc31f8a7f4930a43dc7b99
wuwj <wuwj@spikevision.cn>
2023-05-12 10:00:24 +0800

    sps102 usb demo

---
+++ sps102_usb_demo2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define _AFXDLL
+
+#include "DRV_DriverInterface.h"
+
+#include <atlstr.h>
+#include "APP_Device.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Welcome " <<std::endl;
+    return 0;
+}
